e6c2ae4dbc0ffb80c14074347f74ef2814535428
anchort <anchort@anchorsystemsllc.com>
2022-10-31 15:39:31 -0400

    finalized footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -68,7 +68,7 @@ const ContactForm = () => {
         "Access-Control-Allow-Origin": "*",
       }
     }).then(val => {
-      console.log(val.data.success)
+      alert("Sent email for your contact request.")
       _reCaptchaRef.current.reset()
       formik.resetForm()
     }).catch(e => {
@@ -88,6 +88,7 @@ const ContactForm = () => {
     }
 
     formik.values.captcha = value
+    formik.handleChange
   }
 
 
@@ -231,38 +232,51 @@ const ContactForm = () => {
 // Defines the footer layout
 export default function Footer() {
   return (
-    <div className="min-h-[585px] flex p-8 flex-col lg:flex-row  lg:items-start overflow-hidden">
-      <div className="flex-grow">
-        <div className="overflow-hidden h-24 flex  items-center">
-          <div className="w-56 md:w-72">
-            <Image
-              src={"/ndc-logo-white.png"}
-              width="100%"
-              height="100%"
-              layout="responsive"
-              objectFit="contain"
-              className="-translate-x-4"
-              alt={"ndc logo"}
-            />
+    <div className="min-h-[585px]">
+      <div className="flex p-8 flex-col lg:flex-row  lg:items-start overflow-hidden">
+        <div className="flex-grow flex flex-col">
+          <div className="overflow-hidden h-24 flex  items-center">
+            <div className="w-56 md:w-72">
+              <Image
+                src={"/ndc-logo-white.png"}
+                width="100%"
+                height="100%"
+                layout="responsive"
+                objectFit="contain"
+                className="-translate-x-4"
+                alt={"ndc logo"}
+              />
+            </div>
+          </div>
+          <div className="mt-8 flex flex-col lg:flex-row">
+            <div>
+              <h1 className="font-['Ethnocentric'] text-lg md:text-2xl">Explore</h1>
+              <ul className="footerNavList">
+                <Navlinks />
+              </ul>
+            </div>
+            <div className="relative lg:left-32 space-y-8 mt-8 lg:mt-0">
+              <h1 className="font-['Ethnocentric'] text-lg md:text-2xl">Connect</h1>
+              <BsFacebook style={{ marginTop: 25 }} size={32} />
+              <BsTwitter style={{ marginTop: 25 }} size={32} />
+              <BsLinkedin style={{ marginTop: 25 }} size={32} />
+            </div>
           </div>
         </div>
-        <div className="mt-8 flex flex-col lg:flex-row">
-          <div>
-            <h1 className="font-['Ethnocentric'] text-lg md:text-2xl">Explore</h1>
-            <ul className="footerNavList">
-              <Navlinks />
-            </ul>
-          </div>
-          <div className="relative lg:left-32 space-y-8 mt-8 lg:mt-0">
-            <h1 className="font-['Ethnocentric'] text-lg md:text-2xl">Connect</h1>
-            <BsFacebook style={{ marginTop: 25 }} size={32} />
-            <BsTwitter style={{ marginTop: 25 }} size={32} />
-            <BsLinkedin style={{ marginTop: 25 }} size={32} />
-          </div>
+        <div className="w-full lg:w-1/2 flex justify-end mt-16 lg:mt-0 ">
+          <ContactForm />
         </div>
       </div>
-      <div className="w-full lg:w-1/2 flex justify-end mt-16 lg:mt-0 ">
-        <ContactForm />
+      <div className="flex overflow-hidden px-8 pb-4">
+        <div className="text-[#464646]">
+          <p>4657 Industrial Park Drive</p>
+          <p>Kincheloe, MI 49788</p>
+          <p>906-240-1180</p>
+        </div>
+        <div className="flex-grow"/>
+        <div className="translate-y-6">
+          <Image src={'/anchor-white.png'} width={119} height={35} draggable={false}/>
+        </div>
       </div>
     </div>
   );

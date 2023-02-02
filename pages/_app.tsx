4ef0686072a5d9a467c3746beab58d73bec8cf77
anchort <anchort@anchorsystemsllc.com>
2023-02-02 15:44:58 -0500

    changed to custom google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import '../styles/globals.css'
 import dynamic from 'next/dynamic';
 const Layout = dynamic(() => import('../components/Layout'))
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return( 
       <Layout>
-        <GoogleAnalytics trackPageViews strategy='worker'/>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T1T5JR1FCF"
+          strategy="worker"
+        />
+        <Script id="google-analytics" strategy="worker">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-T1T5JR1FCF');
+          `}
+        </Script>
         <Component {...pageProps} />
       </Layout>
     )
